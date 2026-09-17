35ea2d3f0ac768310507523af559da85e749bd8a
IlBoccia94 <167120420+IlBoccia94@users.noreply.github.com>
2026-09-17 14:01:23 +0200

    Load guest action styles

--- js/guest-ui.js
+++ js/guest-ui.js
@@ -2,6 +2,9 @@ export function buildGuestUi(){
   if(!document.querySelector('link[href^="guest.css"]')){
     const l=document.createElement('link');l.rel='stylesheet';l.href='guest.css?v=20260917-1';document.head.appendChild(l);
   }
+  if(!document.querySelector('link[href^="guest-actions.css"]')){
+    const l=document.createElement('link');l.rel='stylesheet';l.href='guest-actions.css?v=20260917-1';document.head.appendChild(l);
+  }
 
   if(!document.querySelector('.nav-item[data-view="guest"]')){
     const contacts=document.querySelector('.nav-item[data-view="contacts"]');
