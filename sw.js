6d4f8030e4ea5e9778425eb6d91d7f0f60a1df87
IlBoccia94 <167120420+IlBoccia94@users.noreply.github.com>
2026-09-17 03:39:41 +0200

    Use network-only fetch handling for installable PWA

--- sw.js
+++ sw.js
@@ -9,6 +9,11 @@ self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
+self.addEventListener('fetch', event => {
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request));
+});
+
 self.addEventListener('push', event => {
   let payload = {};
   try {
