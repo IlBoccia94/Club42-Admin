3f5f1e20125535573cf8f941b9b2fedfcc1b33a0
IlBoccia94 <167120420+IlBoccia94@users.noreply.github.com>
2026-09-26 17:11:00 +0200

    Bump Guest event RSVP styles

--- js/guest-ui.js
+++ js/guest-ui.js
@@ -1,6 +1,6 @@
 export function buildGuestUi(){
   if(!document.querySelector('link[href^="guest.css"]')){
-    const l=document.createElement('link');l.rel='stylesheet';l.href='guest.css?v=20260926-unregister1';document.head.appendChild(l);
+    const l=document.createElement('link');l.rel='stylesheet';l.href='guest.css?v=20260926-eventrsvp1';document.head.appendChild(l);
   }
   if(!document.querySelector('link[href^="guest-actions.css"]')){
     const l=document.createElement('link');l.rel='stylesheet';l.href='guest-actions.css?v=20260917-1';document.head.appendChild(l);
